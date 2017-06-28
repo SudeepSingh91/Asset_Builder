a6c48e409ede43d48106a090cfd6225fa4da9c82
SudeepSingh <u1016400@utah.edu>
2017-06-28 01:08:53 -0600

    Added files of Project.

---
+++ Code/Tools/EffectBuilder/cEffectBuilder.h
@@ -0,0 +1,33 @@
+#ifndef EAE6320_CEFFECTBUILDER_H
+#define EAE6320_CEFFECTBUILDER_H
+
+// Header Files
+//=============
+
+#include "../AssetBuildLibrary/cbBuilder.h"
+#include "../../External/Lua/Includes.h"
+
+// Class Declaration
+//==================
+
+namespace eae6320
+{
+	namespace AssetBuild
+	{
+		class cEffectBuilder : public cbBuilder
+		{
+			// Inherited Implementation
+			//=========================
+
+		private:
+
+			// Build
+			//------
+
+			virtual bool Build(const std::vector<std::string>& i_arguments);
+		};
+	}
+}
+
+#endif	// EAE6320_CEFFECTBUILDER_H
+
---
+++ Code/Tools/MeshBuilder/cMeshBuilder.h
@@ -0,0 +1,36 @@
+/*
+	This class builds meshes
+*/
+
+#ifndef EAE6320_CMESHBUILDER_H
+#define EAE6320_CMESHBUILDER_H
+
+// Header Files
+//=============
+
+#include "../AssetBuildLibrary/cbBuilder.h"
+#include "../../External/Lua/Includes.h"
+
+// Class Declaration
+//==================
+
+namespace eae6320
+{
+	namespace AssetBuild
+	{
+		class cMeshBuilder : public cbBuilder
+		{
+			// Inherited Implementation
+			//=========================
+
+		private:
+
+			// Build
+			//------
+
+			virtual bool Build( const std::vector<std::string>& i_arguments );
+		};
+	}
+}
+
+#endif	// EAE6320_CMESHBUILDER_H
